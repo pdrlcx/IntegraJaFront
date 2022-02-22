24eb1e8031451b18f278356f4ec0a22c9173821f
1-Cesar <cesar.desenvolvedor@gmail.com>
2022-02-22 03:57:43 -0300

    @1-Cesar [Desenvolvimento de pag. contato]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ContatoComponent } from './contato/contato.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '',redirectTo:'login',pathMatch:'full'},
   {path:'login',component:LoginComponent},
-  {path:'cadastro',component:CadastroComponent}
+  {path:'cadastro',component:CadastroComponent}  
 ];
 
 @NgModule({
