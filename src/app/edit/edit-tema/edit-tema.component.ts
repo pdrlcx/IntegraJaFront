c985cd5857534cb9e78ba75b29d80e8ea6f037cd
Edgar Soares <edemarinho@outlook.com>
2022-03-08 10:13:56 -0300

    @oieusouoede [Correção nos componentes]

---
+++ src/app/edit/edit-tema/edit-tema.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tema',
+  templateUrl: './edit-tema.component.html',
+  styleUrls: ['./edit-tema.component.css']
+})
+export class EditTemaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
