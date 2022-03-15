7d153bc66de36ba5e9d63bc1e06ea8b782c0ad12
pdrlcx <peedrolucass@outlook.com>
2022-03-15 14:28:25 -0300

    @pdrlcx [Acrescentando variaveis novas]

--- src/app/model/Usuario.ts
+++ src/app/model/Usuario.ts
@@ -1,12 +1,19 @@
 import { Postagem } from './Postagem';
 
 export class Usuario {
-  public idUsuario: number;
-  public nomeUsuario: string;
-  public telefoneUsuario: string;
-  public emailUsuario: string;
-  public senhaUsuario: string;
-  public fotoUsuario: string;
+  public id: number;
+  public nome: string;
+  public telefone: string;
+  public email: string;
+  public senha: string;
+  public foto: string;
   public tipoUsuario: string;
+  public biografia: string;
+  public cidade: string;
+  public formacao: string;
+  public areaDeAtuacao: string;
+  public idiomas: string;
+  public certificado: string;
+  public linkedin: string;
   public postagens: Postagem[];
 }
